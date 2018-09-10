5c346b53579433455f6ce713f89a74a9ed43e417
Kaitlin Bunn <kabunn1991@gmail.com>
2018-09-10 11:45:09 -0400

    created user schema

---
+++ models/user.js
@@ -0,0 +1,28 @@
+'use strict';
+
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+
+const userSchema = new mongoose.Schema({
+  username: { type: String, required: true, unique: true },
+  password: { type: String, required: true }
+})
+
+userSchema.set('toObject', {
+  virtuals: true, // built in virtual id
+  versionKey: false, // remove _v
+  transform: (doc, ret) => {
+    delete ret._id, // delete '_id'
+    delete ret.password;
+  }
+})
+
+userSchema.methods.validatePassword = function (password) {
+  return bcrypt.compare(password, this.password);
+}
+
+userSchema.statics.hashPassword = function (password) {
+  return bcrypt.hash(password, 10);
+}
+
+module.exports = mongoose.model('User', userSchema);
