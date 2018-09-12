df770db399f8a4fd919354d34b43d0133be74986
Jonny Cheng <jcheng02@mica.edu>
2018-09-12 11:29:31 -0400

    remove whitespace

--- scratch/index.js
+++ scratch/index.js
@@ -1,13 +1,13 @@
 //test 
 
-'use strict';
-const mongoose = require('mongoose');
-const Question = require('../models/question');
-const User= require('../models/user');
+// 'use strict';
+// const mongoose = require('mongoose');
+// const Question = require('../models/question');
+// const User= require('../models/user');
 
   
-Question.find()
-    .then(result => console.log(result));
+// Question.find()
+//     .then(result => console.log(result));
     
 
 
