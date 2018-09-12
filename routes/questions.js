e3fc1c99fa57932635011a8cd7185308edc0a144
Jonny Cheng <jcheng02@mica.edu>
2018-09-11 21:12:28 -0400

    updated post route

--- routes/questions.js
+++ routes/questions.js
@@ -11,67 +11,67 @@ const User = require('../models/user');
 router.use(passport.authenticate('jwt', { session: false, failWithError: true }));
 
 
-const questions = [
-  {
-    lapine: 'maythennion',
-    english: 'acorn',
-    counter:0
+// const questions = [
+//   {
+//     lapine: 'maythennion',
+//     english: 'acorn',
+//     counter:0
     
-  },
-  {
-    lapine:'dahloi',
-    english:'dandelion'
-  },
-  {
-    lapine:'yera',
-    english:'snow'
-  },
-  {
-    lapine:'syriénnion',
-    english:'strawberry'
-  },
-  {
-    lapine:'firth',
-    english:'sun'
-  },
-  {
-    lapine:'zyz',
-    english:'sun'
-  },
-  {
-    lapine:'efath',
-    english:'plant'
-  },
-  {
-    lapine:'hral',
-    english:'cloud'
-  },
-  {
-    lapine:'hrdudu',
-    english:'mountain'
-  },
-  {
-    lapine:'bryl nos',
-    english:'car'
-  }
-];
+//   },
+//   {
+//     lapine:'dahloi',
+//     english:'dandelion'
+//   },
+//   {
+//     lapine:'yera',
+//     english:'snow'
+//   },
+//   {
+//     lapine:'syriénnion',
+//     english:'strawberry'
+//   },
+//   {
+//     lapine:'firth',
+//     english:'sun'
+//   },
+//   {
+//     lapine:'zyz',
+//     english:'sun'
+//   },
+//   {
+//     lapine:'efath',
+//     english:'plant'
+//   },
+//   {
+//     lapine:'hral',
+//     english:'cloud'
+//   },
+//   {
+//     lapine:'hrdudu',
+//     english:'mountain'
+//   },
+//   {
+//     lapine:'bryl nos',
+//     english:'car'
+//   }
+// ];
 
 
-const linkedList = new LinkedList();
+// const linkedList = new LinkedList();
 
-function loadDummyData(array){
-  for(let i = 0; i < array.length; i++){
-    linkedList.insertFirst(array[i]);
+// function loadDummyData(array){
+//   for(let i = 0; i < array.length; i++){
+//     linkedList.insertFirst(array[i]);
 
   
-  }
-}
+//   }
+// }
 
 
-loadDummyData(questions);
+// loadDummyData(questions);
 
-let curNode = linkedList.head;
-let prevNode= null;
+// let curNode = linkedList.head;
+// let prevNode= null;
 
 
 router.get('/',(req,res,next) =>{
@@ -82,47 +82,72 @@ router.get('/',(req,res,next) =>{
   // console.log('current NODE',curNode);
 
   const userId = req.user.id;
-    User.findById(userId)
+  User.findById(userId)
     .populate('questions.question')
-    .then(user => res.json(user.questions[user.head].question.lapineWord))
+    .then(user =>{
+
+      console.log('CurrentHead',user);
+      res.json(user.questions[user.head].question.lapineWord);
+
+
+    })
     .catch(err => {
-      throw new Error(err);
+      next(err);
     });
       
-  
-
-    // testUser.populate('questions');
-
-    // console.log('USER QUESTIONS',testUser.username);
-
-
-  // if(curNode.next === null){
-  //   curNode.next = linkedList.head;
-  // }
-
-  // let nodes ={
-  //   current: curNode.value,
-  //   previous: prevNode ? prevNode.value : {lapine:'',english:''}
-  // };
-
-
-
-  // res.json(nodes);
-
 });
 
 router.post('/',(req,res,next) =>{
- console.log('user answer req body',req.body);
-  // let {answer} = req.body;
-  console.log('next node?');
-  // if(answer === curNode.value.english){
-  prevNode=curNode;
-  console.log('prevnode',prevNode);
-  curNode=curNode.next;
-    
-  // }
+  console.log('user answer req body',req.body);
+ 
+  const {guess} = req.body;
+  const userId = req.user.id;
+  let answer = null;
 
-  res.json(curNode);
+  User.findById(userId)
+    .populate('questions.question')
+    .then(user => {
+      console.log('REQ USER ID POST',userId); 
+     
+      let currIndex = user.head;
+      let correctCount = user.questions[currIndex].correctAnswers;
+      let correctAnswer = user.questions[currIndex].question.englishWord;
+
+      //increment attempt counter
+      user.questions[currIndex].attempts += 1;
+
+      console.log('correct count',correctCount);
+      let correctGuess;
+      console.log('answer',answer,'guess',guess); 
+     
+      if(answer === guess){
+        console.log('CORRECT ANSWER');
+        correctGuess = true;
+        user.questions[currIndex].correctAnswers +=1;
+
+      }
+      else{
+        console.log('Wrong ANSWER');
+        correctGuess = false;
+        
+      }
+      let nextNode = user.questions[currIndex].next;
+      user.head = nextNode;
+    
+      user.save(function (err, updatedUser) {
+        if (err) {
+          next(err);
+        }
+        if(!correctGuess){
+          res.json(user.questions[currIndex].question.englishWord);
+        } else {
+          res.status(200);
+        }
+
+      });
+
+
+    });
 
 
 });
